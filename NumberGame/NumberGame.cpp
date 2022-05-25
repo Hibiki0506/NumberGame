41420839ed08c7121e05c88d57e31570a36d844d
Hibiki0506 <david@ASUS_ROG_C6H>
2022-05-25 21:15:37 +0200

    Added icon, fixed errors, using rand() instead of random_device, etc..

--- NumberGame/NumberGame.cpp
+++ NumberGame/NumberGame.cpp
@@ -14,36 +14,36 @@ int main() {
 	int Loop = 0;
 	bool Condition = false;
 
-	
+
 	//Random number generator start
-	srand((unsigned int) time(0));
+	srand((unsigned int)time(0));
 
 	//generate random number
 	CPU = rand() % (100 + 1 - 1) + 1;
 
 	//cout << CPU; *debug output
-	
+
 
 	//Player instruction output
 	cout << "Hello!\n\n";
 	cout << "The CPU generates a number between 1 and 100.\n";
 	cout << "Guess the number which the CPU has picked.\n";
-	cout << "If you guess the number correctly within 10 tries, you win!";
+	cout << "If you guess the number correctly within 10 tries, you win!\n\n";
 
 	//Game start
 	do {
 		//cout << Loop << ". try\n"; // *debug output
 		Loop++;
-		cout << "Type in your number:";
+		cout << "Type in your number:\n";
 		cin >> Player;
 		if (Player == CPU)
 		{
-			cout << "You are the winner!\n";
+			cout << "You are the winner!\n\n";
 			Condition = true;
 		}
 		else if (Player > CPU)
 		{
-			cout << "The number you are looking for is smaller. Please try again.\n";
+			cout << "The number you are looking for is smaller. Please try again.\n\n";
 		}
 		else if (Loop == 10)
 		{
@@ -52,26 +52,28 @@ int main() {
 		}
 		else
 		{
-			cout << "The number you are looking for is bigger. Please try again.\n";
+			cout << "The number you are looking for is bigger. Please try again.\n\n";
 		}
 	} while (Condition == false);
 
 	//Game won/lost?
 	if (Loop <= 10)
 	{
-		cout << "You took " << Loop << " tries and managed to find the correct number.\n";
-		cout << "\nCongratulations!\n" << "If you want to play again, please click on this programs' file again.\n";
+		cout << "Congratulations!\n\n";
+		cout << "You took " << Loop << " tries and managed to find the correct number.\n\n";
+		cout << "If you want to play again, please click on this programs' file again.\n\n";
 	}
 	else if (Loop == 11)
 	{
-		cout << "You took " << Loop-- << " tries, but you didn't managed to find the correct number.\n";
-		cout << "\nAww, too bad!\n" << "If you want to try again, please click on this programs' file again.\n";
+		cout << "Aww, too bad!\n\n";
+		cout << "You took " << Loop - 1 << " tries, but you didn't managed to find the correct number.\n\n";
+		cout << "If you want to try again, please click on this programs' file again.\n\n";
 	}
 
 	//Game end
-	cout << "\nThank you for playing!\n\n";
+	cout << "Thank you for playing!\n\n";
 
-	cout << "\nPress the ENTER key to exit...\n";
+	cout << "Press the ENTER key to exit...\n";
 
 	cin.ignore();
 	cin.get();
